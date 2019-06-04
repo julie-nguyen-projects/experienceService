fe76f605e9fef92fd310a9b74328dc95e6a7aa99
julie-nguyen-projects <julie_nguyen@live.fr>
2019-06-04 11:36:10 +0200

    feat(school): school must be unique to be created

--- src/main/java/com/epitech/pgt2019/repository/SchoolRepository.java
+++ src/main/java/com/epitech/pgt2019/repository/SchoolRepository.java
@@ -1,11 +1,11 @@
 package com.epitech.pgt2019.repository;
 
 import com.epitech.pgt2019.domain.School;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -17,4 +17,5 @@ public interface SchoolRepository extends MongoRepository<School, String> {
 
     List<School> findByNameContainsIgnoreCase(String name);
 
+    Optional<School> findByNameIgnoreCaseAndCityExp(String name, String cityId);
 }
